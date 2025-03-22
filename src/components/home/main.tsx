0eeffb9c725957f8b27b462f3a78e9ea22034085
ctycho-dev <ctycho23@gmail.com>
2025-03-22 15:47:02 +0300

    deploy v6

--- src/components/home/main.tsx
+++ src/components/home/main.tsx
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { Button } from "@mantine/core";
+import React, { useState, useEffect, memo } from "react";
 import Title from '@/components/title'
 import Subtitle from '@/components/subtitle'
 import CustomButton from "@/components/customButton";
-
-import dashboard1 from '@/assets/home/main/dashboard-1x.png'
-import dashboard2 from '@/assets/home/main/dashboard-2x.png'
-import dashboard4 from '@/assets/home/main/dashboard-4x.png'
-import mdash1 from '@/assets/home/main/mdash-1x.png'
-import mdash2 from '@/assets/home/main/mdash-2x.png'
-import mdash4 from '@/assets/home/main/mdash-4x.png'
-import circle from '@/assets/circle.png'
-import circle3 from '@/assets/home/main/circle-1x.png'
 
 interface MainProps {
     scrollToWishlist: () => void
 }
 
 
-export const Main: React.FC<MainProps> = ({ scrollToWishlist }) => {
+export const Main: React.FC<MainProps> = memo(({ scrollToWishlist }) => {
     const [isButtonVisible, setIsButtonVisible] = useState(false);
 
     useEffect(() => {
@@ -53,11 +43,31 @@ export const Main: React.FC<MainProps> = ({ scrollToWishlist }) => {
                 <CustomButton className="z-20 sticked-btn" handler={scrollToWishlist} >Join the Waitlist Now</CustomButton>
             </div>
             <div className="relative md:static w-full flex justify-center">
-                <img src={dashboard4} alt="" srcSet={`${dashboard1} 1x, ${dashboard2} 2x, ${dashboard4} 4x`} className="hidden md:block" />
-                <img src={mdash4} srcSet={`${mdash1} 1x, ${mdash2} 2x, ${mdash4} 4x`} alt="" className="md:hidden w-full" />
-                <img src={circle} alt="" className="hidden md:block absolute -z-10 left-1/2 top-52 md:top-72 -translate-x-1/2 -translate-y-1/2 max-w-full md:max-w-3xl" />
-                <img src={circle3} alt="" className="md:hidden absolute -z-10 left-0 right-0 w-full -top-1/4 scale-125" />
+                {/* Desktop Dashboard */}
+                <picture className="hidden md:block">
+                    <source srcSet="https://link.storjshare.io/raw/jxcu7h6ekk5nnbvuozrdvlagpn5q/athenax/main/dashboard-2x.webp 2x, https://link.storjshare.io/raw/jvf3wrr444zuojs5ropfpgjcxsdq/athenax/main/dashboard-4x.webp 3x" type="image/webp" />
+                    <source srcSet="https://link.storjshare.io/raw/jv6pwbryumbrh4exwx62mmofzyva/athenax/main/dashboard-2x.png 2x, https://link.storjshare.io/raw/juqgij4hyhdqpz2vyg5iwzpgiq5a/athenax/main/dashboard-4x.png 3x" type="image/png" />
+                    <img src="https://link.storjshare.io/raw/juid2k5cvcmqgxiom2mrazlnokwq/athenax/dashboard-4x.png" srcSet="https://link.storjshare.io/raw/jv6pwbryumbrh4exwx62mmofzyva/athenax/main/dashboard-2x.png 2x, https://link.storjshare.io/raw/juqgij4hyhdqpz2vyg5iwzpgiq5a/athenax/main/dashboard-4x.png 3x" alt="Dashboard" />
+                </picture>
+                {/* Mobile Dashboard */}
+                <picture className="md:hidden w-full">
+                    <source srcSet="https://link.storjshare.io/raw/jxuf3w4gbitvjmy4d73yvzn6v7fa/athenax/main/mdash-2x.webp 2x, https://link.storjshare.io/raw/juylot4hmsoa7eluckw6e34lbfoq/athenax/main/mdash-4x.webp 3x" type="image/webp" />
+                    <source srcSet="https://link.storjshare.io/raw/jwcgimkwfgcly6sikoa3an4trrga/athenax/main/mdash-2x.png 2x, https://link.storjshare.io/raw/jwdep2aj5s6k5kogrwxcok5bppwa/athenax/main/mdash-4x.png 3x" type="image/png" />
+                    <img src="https://link.storjshare.io/raw/jwdep2aj5s6k5kogrwxcok5bppwa/athenax/main/mdash-4x.png" srcSet="https://link.storjshare.io/raw/jwcgimkwfgcly6sikoa3an4trrga/athenax/main/mdash-2x.png 2x, https://link.storjshare.io/raw/jwdep2aj5s6k5kogrwxcok5bppwa/athenax/main/mdash-4x.png 3x" alt="Dashboard" />
+                </picture>
+                {/* Desktop Gradient */}
+                <picture className="hidden md:block absolute -z-10 left-1/2 top-52 md:top-72 -translate-x-1/2 -translate-y-1/2 max-w-full md:max-w-3xl">
+                    <source srcSet="https://link.storjshare.io/raw/jud5sldjbjeceowcua5gkd2wy5za/athenax/main/circle.webp" type="image/webp" />
+                    <source srcSet="https://link.storjshare.io/raw/jvds2zvojcry6qnhimc4ijbhii3a/athenax/main/circle.png" type="image/png"  />
+                    <img src="https://link.storjshare.io/raw/jvds2zvojcry6qnhimc4ijbhii3a/athenax/main/circle.png" alt="" />
+                </picture>
+                {/* Mobile Gradient */}
+                <picture className="md:hidden absolute -z-10 left-0 right-0 w-full -top-1/4 scale-125">
+                    <source srcSet="https://link.storjshare.io/raw/jvfkgkmazgf3gru5ijfarc5o3waq/athenax/main/mcircle.webp" type="image/webp" />
+                    <source srcSet="https://link.storjshare.io/raw/jwk5552zlcanxzurhq6ur4dmwoiq/athenax/main/mcircle.png" type="image/png"  />
+                    <img src="https://link.storjshare.io/raw/jwk5552zlcanxzurhq6ur4dmwoiq/athenax/main/mcircle.png" alt="" />
+                </picture>
             </div>
         </main>
     )
-} 
+})
--- src/components/home/reports.tsx
+++ src/components/home/reports.tsx
@@ -7,11 +7,8 @@ import CardTitle from '@/components/cardTitle'
 import CardSubtitle from "../cardSubtitle";
 import Card from "@/components/card";
 
-import trends from '@/assets/home/reports/trends.png'
 import gradient from '@/assets/home/reports-gradient.png'
-
 import vip from '@/assets/home/reports/vip.svg'
-import pro from '@/assets/home/reports/pro.webm'
 
 interface ReportsProps { }
 
@@ -37,7 +34,13 @@ export const Reports: React.FC<ReportsProps> = ({ }) => {
                 </div>
                 <div className="flex flex-col md:grid md:grid-cols-2 gap-6">
                     <Card className="p-6 md:p-10 flex flex-col justify-between">
-                        <div className="flex justify-center"><img loading="lazy" src={trends} alt="" /></div>
+                        <div className="flex justify-center">
+                            <picture>
+                                <source srcSet="https://link.storjshare.io/raw/jwzs7c7fxiykxc3gurje4efqcnja/athenax/reports/trends.webp" type="image/webp" />
+                                <source srcSet="https://link.storjshare.io/raw/jv3kx6pfyeaggcpiy4ojdmsqtfra/athenax/reports/trends.png" type="image/png" />
+                                <img src="https://link.storjshare.io/raw/jv3kx6pfyeaggcpiy4ojdmsqtfra/athenax/reports/trends.png" srcSet="https://link.storjshare.io/raw/jv3kx6pfyeaggcpiy4ojdmsqtfra/athenax/reports/trends.png" alt="Dashboard" />
+                            </picture>
+                        </div>
                         <div className="pt-6">
                             <CardTitle className="mb-2">Expert Insights & Market Trends</CardTitle>
                             <CardSubtitle className="w-2/3">Access quarterly reports with deep dives into blockchain ecosystems, tokenomics, and key market shifts</CardSubtitle>
@@ -46,7 +49,7 @@ export const Reports: React.FC<ReportsProps> = ({ }) => {
                     <Card className="p-6 md:p-10 flex flex-col justify-between">
                         <div className="flex-1 flex justify-center overflow-hidden">
                             <video controls width="100%" autoPlay loop playsInline preload="true" muted className="scale-200 py-4">
-                                <source src={pro} type="video/webm" />
+                                <source src='https://link.storjshare.io/raw/jvrfafnej3dpagurq6v2s6hso5na/athenax/reports/pro.webm' type="video/webm" />
                                 Your browser does not support the video tag.
                             </video>
                             {/* <iframe src="https://unicorn.studio/embed/ogeen1Qu7P7Q9noHiKtg" width={1200} className="scale-[195%] h-[280px] md:h-[310px]"></iframe> */}
