93aa8b7959ad0b86b59af2832a85b1f08cbf7004
ctycho-dev <ctycho23@gmail.com>
2025-04-08 23:28:59 +0300

    forms are implemented v4

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: process.env.VITE_APP_API_BASE_URL || 'http://localhost:8000/api/v1',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
