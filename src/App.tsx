34ae4dd8319233c0ef20c24651340284cc35d587
ctycho-dev <ctycho23@gmail.com>
2025-10-27 04:11:28 +0300

    footer changed v2

--- src/App.tsx
+++ src/App.tsx
@@ -36,12 +36,53 @@ function App() {
     // }
 
     return (
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<Home />} />
+        //     </Routes>
+        // </Router>
+
         <Router>
+            <PathTracker />
             <Routes>
+                {/* Public Routes */}
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route path="/article/:slug" element={<WriteArticle />} /> */}
+                {/* <Route path="/" element={<UserLayout />}>
+                    <Route path="/top-cryptos" element={<CryptoList />} />
+                </Route> */}
+
+                <Route element={<AuthOnlyWrapper />}>
+                    <Route path="/welcome" element={<Welcome />} />
+                </Route>
+
+                {/* Private Routes */}
+                {/* <Route path="/" element={<AuthWrapper allowedRoles={[UserRole.ADMIN, UserRole.BD]} />}> */}
+                <Route path="/" element={<AuthWrapper allowedRoles={[]} />}>
+                    <Route path="/" element={<Layout />}>
+                        <Route path="/article/new" element={<WriteArticle key="write-new" />}  />
+                        <Route path="/article/:id/edit" element={<WriteArticle key="edit" />} />
+                        <Route path="/article/me" element={<MyArticles />} />
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/submit_materials" element={<SumbitMaterials />} />
+                    </Route>
+                    {/* <Route path="/submit/audit" element={<AuditForm />} />
+                    <Route path="/submit/audit/:id" element={<AuditForm />} /> */}
+                    <Route path="/submit/research" element={<ResearchForm />} />
+                    <Route path="/submit/research/:id" element={<ResearchForm />} />
+                    <Route path="/submited" element={<Submited />} />
+                </Route>
+                <Route path='/' element={<AuthWrapper allowedRoles={[UserRole.BD, UserRole.ADMIN]} />}>
+                    <Route path="/" element={<Layout />}>
+                        {/* <Route path="/review/audit" element={<AuditReview />} />
+                        <Route path="/review/audit/:id" element={<AuditReviewRecord />} /> */}
+                        <Route path="/review/research" element={<ResearchReview />} />
+                        <Route path="/review/research/:id" element={<ResearchReviewRecord />} />
+                    </Route>
+                </Route>
             </Routes>
         </Router>
-
     )
 }
 
